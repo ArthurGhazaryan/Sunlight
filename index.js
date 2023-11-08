1eb35c8a930849d78d7cbf9b0d87d4a3fffbd2cd
ArthurGhazaryan <82112268+ArthurGhazaryan@users.noreply.github.com>
2023-11-08 12:39:55 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let moon = document.getElementById("moonDiv");
+let sun = document.getElementById("sunDiv");
+let night = function(){
+    sun.style.transition = "6s";
+    sun.style.marginTop = "-80px";
+    sun.style.backgroundColor = "rgb (236, 226, 76)";
+    moon.style.transition = "8s";
+    moon.style.marginLeft = "120px";
+    document.body.style.transition = "15s";
+    document.body.style.backgroundColor = "#e4e4ff";
+}
+
+setTimeout(()=>{
+    night()
+}, 1000
+)
